85c8e28d78d3e2a0eae6e87fc0ea1d6ab95b480c
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-01-17 17:43:18 -0500

    changed to pointer pointers

--- arraylist.c
+++ arraylist.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "arraylist.h"
 
@@ -13,25 +14,40 @@ arraylist* arraylist_new(int size){
 
     return arr_ls;
 }
-void arraylist_free(arraylist *arr_ls){
-    free(arr_ls->_arr);
-    free(arr_ls);
+void arraylist_free(arraylist **arr_ls){
+    free((*arr_ls)->_arr);
+    free((*arr_ls));
 }
-void arraylist_add(arraylist *arr_ls, int element){
-    arr_ls->_arr[arr_ls->_c_idx] = element; // change the value pointed to by array pointer
-    arr_ls->_c_idx++; //we've moved to a new location
+void arraylist_add(arraylist **arr_ls, int element){
+    (*arr_ls)->_arr[(*arr_ls)->_c_idx] = element; // change the value pointed to by array pointer
+    (*arr_ls)->_c_idx++; //we've moved to a new location
 }
-int arraylist_get(arraylist *arr_ls, int index){
-    return arr_ls->_arr[index];
+int arraylist_get(arraylist **arr_ls, int index){
+    return (*arr_ls)->_arr[index];
 }
 
-void _arraylist_double(arraylist *arr_ls){
-    arraylist *arr_ls_new = arraylist_new(arr_ls->size * 2); //double the size
-    int i;
-    for (i = 0; i < arr_ls->_c_idx; i++)
-        arraylist_add(arr_ls_new, arraylist_get(arr_ls, i)); // copy everything over
+/*void _arraylist_double(arraylist *arr_ls){*/
+    /*// *arr_ls; //the struct pointed to*/
+    /*// &arr_ls; //the address of the pointer of ls*/
 
-    arraylist *address_store = arr_ls;
-    *arr_ls = *arr_ls_new;
-    /*arraylist_free(address_store);*/
-}
+    /*[> double the array size and copy everyting over <]*/
+    /*arraylist *arr_ls_new = arraylist_new(arr_ls->size * 2); //double the size*/
+    /*int i;*/
+    /*for (i = 0; i < arr_ls->_c_idx; i++)*/
+        /*arraylist_add(arr_ls_new, arraylist_get(arr_ls, i)); // copy everything over*/
+    /*[> -end comment- <]*/
+
+    /* we want the pointer passed in to point to a different struct after the function exits*/
+        /**arr_ls = *arr_ls_new; //this is how we do that*/
+        /*but we don't want to do that because then we won't be able to free the memory that that pointer originall pointed to*/
+    /**/
+    /*arraylist_free(arr_ls);*/
+    /*[>arr_ls = arr_ls_new;<]*/
+
+    /*[>arraylist address_store = *arr_ls;<]*/
+    /*[>arr_ls_new = &address_store;<]*/
+    /*[>printf("size of address_store %d\n", address_store.size);<]*/
+    /*[>free(arr_ls_new->_arr);<]*/
+    /*[>free(arr_ls_new);<]*/
+    /*[>arraylist_free(arr_ls_new);<]*/
+//}
--- main.c
+++ main.c
@@ -5,11 +5,13 @@ int main(){
     arraylist *arr_ls = arraylist_new(100);
     int i;
     for (i = 0; i < arr_ls->size; i++)
-        arraylist_add(arr_ls, i * 20);
+        arraylist_add(&arr_ls, i * 20);
 
-    _arraylist_double(arr_ls);
+    /*_arraylist_double(arr_ls);*/
 
-    printf("%d\n", arr_ls->size);
-    arraylist_free(arr_ls);
+    printf("%d\n", arraylist_get(&arr_ls, 11));
+    arraylist_free(&arr_ls);
+    /*printf("%d\n", arraylist_get(arr_ls, 98));*/
+    /*arraylist_free(arr_ls);*/
     return 0;
 }
